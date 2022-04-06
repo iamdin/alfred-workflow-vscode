24a830382437e2857e6530c71443689e7e00e6fd
jinyang <djy.dingjinyang@gmail.com>
2022-04-06 12:07:48 +0800

    feat: open with vscode

---
+++ README.md
@@ -0,0 +1,31 @@
+# Alfred workflow for VS Code CLI
+
+An Alfred 4 workflow for Visual Studio Code Command Line Interface (CLI).
+
+![showcase](./showcase.png)
+
+## Feature
+
+- 📂 Open Folder with VS Code
+- and more ...
+
+## Installation
+
+1. Download the workflow
+
+2. Double click the .alfredworkflow file to install
+
+Note that the Alfred Powerpack is required to use workflows.
+
+## Requirement
+
+- Nodejs
+- Visual Studio Code Command Line
+
+## Usage
+
+### Search & Open
+
+1. Use the keyword `codef` to trigger the workflow
+2. Type your folder to search
+3. Select the folder you want to open and press enter
---
+++ index.mjs
@@ -0,0 +1,32 @@
+import parseArgs from 'minimist'
+import { globby } from 'globby'
+
+const CODEDIR_PATH = process.env.CODEDIR_PATH ?? process.env.HOME
+const argv = parseArgs(process.argv.slice(2))
+
+const query = argv._.map((e) => `**/*${e}*`)
+
+let dirs = await globby([...(query.length ? query : '**')], {
+  onlyDirectories: true,
+  // onlyFiles: false,
+  ignore: ['**/node_modules/**'],
+  cwd: CODEDIR_PATH,
+  absolute: true,
+  objectMode: true,
+  // stats: true,
+  // deep: 1,
+})
+
+const items = dirs.map(({ name, path }) => ({
+  uid: name,
+  type: 'file',
+  title: name,
+  subtitle: path,
+  arg: path,
+  icon: {
+    type: 'fileicon',
+    path: path,
+  },
+}))
+
+console.log(JSON.stringify({ items }))
